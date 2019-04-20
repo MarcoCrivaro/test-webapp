924bfc3f6747655e1f4016861c9799bbd77ebc01
MarcoCrivaro <marcocrivaro@gmail.com>
2019-04-20 18:13:24 +0200

    first draft complete

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,19 +11,22 @@ def index():
 
 @app.route('/api/turnOn')
 def turnOn():
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setup(2, GPIO.OUT)
-    #GPIO.output(2, GPIO.HIGH)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+    GPIO.output(2, GPIO.HIGH)
     return "Lampe eingeschaltet."
 
 @app.route('/api/turnOff')
 def turnOff():
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setup(2, GPIO.OUT)
-    #GPIO.output(2, GPIO.LOW)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+    GPIO.output(2, GPIO.LOW)
     return "Lampe ausgeschaltet."
 
 @app.route('/api/toggle')
 def toggle():
     #yet to implement
     return "Lampe toggled."
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
